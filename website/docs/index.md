6bdbd3f19ccd01652424578a9292beb0d1073f4e
berlin <1580940252@qq.com>
2024-05-14 16:04:59 +0800

    docs: init website

---
+++ website/docs/index.md
@@ -0,0 +1,18 @@
+---
+pageType: home
+
+hero:
+  name: Pareto
+  text: is a SSR framework centered on stream rendering.
+  tagline: Easy, Super Fast
+  actions:
+    - theme: brand
+      text: Quick Start
+      link: /guide/start/quick-start
+    - theme: alt
+      text: Github
+      link: https://github.com/web-infra-dev/rspress
+  image:
+    src: /rspress-icon.png
+    alt: Pareto Logo
+---
---
+++ website/rspress.config.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'Pareto',
+  description: 'Pareto is an SSR (Server Side Rendering) framework centered on stream rendering.',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+    ],
+  },
+});
